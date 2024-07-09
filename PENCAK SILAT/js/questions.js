13f3baf8b6685fec9d5ed8e5f39a10e86d8904fb
BaimJAV <135565590+BaimJAV@users.noreply.github.com>
2024-07-09 20:48:27 +0700

    Add files via upload

---
+++ PENCAK SILAT/js/questions.js
@@ -0,0 +1,63 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Induk organisasi pencak silat Indonesia adalah…..?",
+    answer: "IPSI",
+    options: [
+      "Silatnas",
+      "IPSI",
+      " PPSI",
+      "IPSSI",
+      " Persinas"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Berikut yang bukan teknik gerak dasar pencak silat adalah …..?",
+    answer: " Smash",
+    options: [
+      "Jatuhan",
+      "Pukulan",
+      "Tendangan",
+      "Tangkisan",
+      " Smash"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Dalam pencak silat teknik tendangan yang masuk sasaran mendapat nilai …..?",
+    answer: "2",
+    options: [
+      "1",
+      "2",
+      "3",
+      "4",
+      "5"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Dalam pencak silat teknik pukulan yang masuk sasaran mendapat nilai.........?",
+    answer: "1 ",
+    options: [
+      "1",
+      "2",
+      "3",
+      "4",
+      "5"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Berikut contoh unsur-unsur pasang dalam pencak silat, kecuali....?",
+    answer: "Pasang atas",
+    options: [
+      "Pasang atas",
+      "Pasang sejajar",
+      "Pasang kanan depan",
+      "Pasang kiri depan",
+      "Pasang samping kiri"
+    ]
+ },
+];
